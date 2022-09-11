4c382607597be223b168348e2a8ef9c7d955851e
Victor Okonkwo <emmyvic98@gmail.com>
2022-09-11 14:44:03 +0100

    tic tac board set

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let tileKey = 1;
+
+const Board = () => {
+  return (
+    <div className="board">
+      <Row />
+      <Row />
+      <Row />
+    </div>
+  );
+};
+const Row = () => {
+  return (
+    <div className="row">
+      <Tiles/>
+      <Tiles />
+      <Tiles />
+    </div>
+  );
+};
+const Tiles = () => {
+  return <div className="tiles">{tileKey++}</div>;
+};
+
+ReactDOM.render(<Board />, document.getElementById("root"));
